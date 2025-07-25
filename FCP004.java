458fbcac0366e81dbea4f78671c06b2b133034c6
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-07-25 19:18:20 +0530

    FCP004

---
+++ FCP004.java
@@ -0,0 +1,12 @@
+public class FCP004{
+public static void main(String a[])
+{
+int b=12;
+int c=5;
+System.out.println(b+c);
+System.out.println(b-c);
+System.out.println(b*c);
+System.out.println((float)((float)b/c));
+System.out.println(b/c);
+System.out.println(b%c);
+}}
