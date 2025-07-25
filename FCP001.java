9a208e114672f04d0804433d374f6183dcab0010
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-07-25 19:15:28 +0530

    FCP001

---
+++ FCP001.java
@@ -0,0 +1,10 @@
+public class FCP001{
+public static void main(String a[])
+{
+int day=5;
+String month="aug";
+int year=2005;
+System.out.println(day);
+System.out.println(month);
+System.out.println(year);}
+}
