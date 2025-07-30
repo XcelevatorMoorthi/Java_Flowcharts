05f2f6b2f8ca4ad60e775fcc545d32f9e7aaf343
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-07-30 09:43:19 +0530

    Add files via upload

---
+++ FCP012.java
@@ -0,0 +1,14 @@
+public class FCP012{
+public static void main(String args[])
+{
+int x=-12;
+if(x>0)
+{System.out.println("positive");
+}
+else if(x==0){
+System.out.println("zero");
+}
+else{
+System.out.println("negative");
+}
+}}
