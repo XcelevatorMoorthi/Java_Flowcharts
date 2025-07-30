cf588900729d5f35d1d0d9eca055f537b55eea37
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-07-30 09:43:46 +0530

    Add files via upload

---
+++ FCP014.java
@@ -0,0 +1,18 @@
+public class FCP014{
+public static void main(String a[]){
+int max=0;
+int x=12;
+int y=13;
+int z=14;
+if(x>y){
+max=x;
+}
+else{
+max=y;
+}
+if(max>z){
+System.out.println(max);
+}
+else{
+System.out.println(z);
+}}}
