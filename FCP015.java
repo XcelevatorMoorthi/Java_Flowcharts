c461523934ee987773d19f7c48ec12dedca63ce0
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-07-30 09:43:58 +0530

    Add files via upload

---
+++ FCP015.java
@@ -0,0 +1,27 @@
+public class FCP015{
+public static void main(String a[]){
+int max=0;
+int max1=0;
+int x=90;
+int y=20;
+int z=80;
+int k=30;
+if(x>y){
+max=x;
+}
+else{
+max=y;
+}
+if(max>z){
+max1=max;
+}
+else{
+max1=z;
+}
+if(max1>k){
+System.out.println(max1);
+}
+else{
+System.out.println(k);
+}}}
+
