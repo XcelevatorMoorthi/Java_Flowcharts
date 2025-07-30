273f6d209a06e60519e30fff3732526365f24818
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-07-30 09:44:38 +0530

    Add files via upload

---
+++ FCP017.java
@@ -0,0 +1,27 @@
+public class FCP017{
+public static void main(String[]arg)
+{
+int min=0;
+int max=0;
+int a=5;
+int b=4;
+int c=6;
+if(a<b)
+{
+min=a;
+max=b;
+}
+else{
+min=b;
+max=a;
+}
+if(max<c)
+{
+System.out.println(max);
+}
+else{
+System.out.println(c);
+}
+}}
+
+
