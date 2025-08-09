2be159ede86e107d182541efd4886db894edfdf3
XcelevatorMoorthi <Ponmoorthi.2520@xcelevate.org>
2025-08-09 08:41:22 +0530

    Add files via upload

---
+++ FCP049.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class FCP049
+{
+  public static void main(String[]args)
+  {
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     int s=0;
+     for(int i=1;i<=n;i++)
+     {
+       int c=0;
+       for(int j=1;j<=n;j++)
+       {
+         if(i%j==0)
+         {
+	   c=c+1;
+          
+          }
+         else{
+            }
+	}
+      if(c==2)
+       {
+          s=s+1;
+        }
+       else
+         {
+	   }
+       }
+	System.out.println(s);
+
+}
+}
+   
+          
